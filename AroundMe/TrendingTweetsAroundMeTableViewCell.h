092936691c829ff67c758278c41302062d429181
Lohit VijayaRenu <lvijayarenu@twitter.com>
2013-04-02 15:08:16 -0700

    Got two Table view working. Tweets around me and Trending topics tweets around me in another Tab.

---
+++ AroundMe/TrendingTweetsAroundMeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TrendingTweetsAroundMeTableViewCell.h
+//  AroundMe
+//
+//  Created by Lohit VijayaRenu on 4/2/13.
+//  Copyright (c) 2013 Lohit VijayaRenu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrendingTweetsAroundMeTableViewCell : UITableViewCell
+
+@end
---
+++ AroundMe/TrendingTweetsAroundMeTableViewCell.m
@@ -0,0 +1,29 @@
+//
+//  TrendingTweetsAroundMeTableViewCell.m
+//  AroundMe
+//
+//  Created by Lohit VijayaRenu on 4/2/13.
+//  Copyright (c) 2013 Lohit VijayaRenu. All rights reserved.
+//
+
+#import "TrendingTweetsAroundMeTableViewCell.h"
+
+@implementation TrendingTweetsAroundMeTableViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
